4684ca373e7617999caa61e689f6710e5b158693
eliel <elieldzik@outlook.fr>
2024-12-30 10:58:32 +0100

    Ajout de contraintes pour casser les symétries + tentatives de reconstruction de chemin

--- statique.jl
+++ statique.jl
@@ -13,7 +13,7 @@ function statique(file_name::String)
 
     @variable(m, x[1:n, 1:n, 1:n], Bin);
     @variable(m, y[1:n], Bin);
-    @variable(m, 0 <= u[1:n, 1:n] <= n, Int);
+    @variable(m, 0 <= u[1:n, 1:n] <= n);
 
     @objective(m,Min, sum(x[i,j,k]*t[i,j] for i in 1:n, j in 1:n, k in 1:n))
 
@@ -27,6 +27,8 @@ function statique(file_name::String)
 
     @constraint(m, sum(x[1,j,k] for k in 1:n, j in 2:n) == sum(y[k] for k in 1:n))
     @constraint(m, sum(x[i,1,k] for k in 1:n, i in 2:n) == sum(y[k] for k in 1:n))
+    @constraint(m,[k in 1:n-1], y[k] >= y[k+1])
+    @constraint(m, sum(x[1,1,k] for k in 1:n) == 0)
 
     optimize!(m)
 
@@ -35,7 +37,22 @@ function statique(file_name::String)
         # Récupération des valeurs d’une variable
         println("Valeur de l’objectif : ", JuMP.objective_value(m))
         println("Nombre de véhicules : ", JuMP.value(sum(y[k] for k in 1:n)))
+        # Reconstruction des chemins
+       for k in 1:n
+            i = 1
+            j0 = 0
+            while j0 != 1
+                for j in 1:n
+                    if value(x[i,j,k]) == 1
+                        println("Véhicule ", k, " : ", i, " -> ", j)
+                        i = j
+                        j0 = j
+                        break
+                    end
+                
+                
+            end
+        end
     end
 end
-    
-  
+end
