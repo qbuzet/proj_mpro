54ff7282b01cfa22cd132c927a4be1f49d0b5bc7
eliel <elieldzik@outlook.fr>
2025-02-11 09:47:52 +0100

    version non compacte et nouveaux résultats en statique

--- dualisation_non_compacte.jl
+++ dualisation_non_compacte.jl
@@ -1,6 +1,8 @@
 using JuMP
-#using CPLEX
-using GLPK
+using CPLEX
+#using GLPK
+
+
 
 function robuste_dualisation_non_compacte(file_name::String)
     file_acces = "./data/" * file_name
@@ -12,11 +14,13 @@ function robuste_dualisation_non_compacte(file_name::String)
     
     # Créer le modèle
     m = Model(CPLEX.Optimizer)
+
     #m = Model(GLPK.Optimizer)
     
        
 
     @variable(m, x[1:n, 1:n], Bin);
+    @variable(m, 0 <= u[1:n]);
 
     @variable(m, lambda_1 >= 0 );
     @variable(m, lambda_2 >= 0 );
@@ -38,27 +42,74 @@ function robuste_dualisation_non_compacte(file_name::String)
                     
                    
     @constraint(m, sum(x[i,i] for i in 1:n) == 0) # on ne peut pas aller de i à i
-                        
+
+    
+    @constraint(m, [i in 2:n,j in 2:n, i!=j], u[j] - u[i] >= d[i] - C*(1-x[i,j]))
+
    
 
     @constraint(m, [j in 1:n, i in 1:n, i!=j], (th[i] + th[j]) * x[i,j] <= lambda_1 + alpha[i,j] )
     @constraint(m, [j in 1:n, i in 1:n, i!=j], (th[i] * th[j]) * x[i,j] <= lambda_2 + beta[i,j] )
 
+
+        # Ajout dynamique des contraintes de sous-tour (SEC)
+    function add_subtour_constraints(m)
+        sol = value.(x)  # Solution courante
+        visited_nodes = [i for i in 2:n if sum(sol[i, j] for j in 1:n) > 0.5]
+
+        if length(visited_nodes) > 1
+            # Si sous-tour détecté, on l'interdit
+            @constraint(m, sum(x[i, j] for i in visited_nodes, j in visited_nodes) <= length(visited_nodes) - 1)
+        end
+    end
+
+    # Résolution avec ajout dynamique des SEC
+    iteration = 0
+    while true
+        iteration += 1
+        optimize!(m)
+        
+        prev_obj = objective_value(m)
+        add_subtour_constraints(m)  # Ajout des SEC
+
+        optimize!(m)  
+        new_obj = objective_value(m)
+
+        if abs(prev_obj - new_obj) < 1e-5  # Si convergence
+            break
+        end
+    end
+
+
     optimize!(m)
     return JuMP.objective_value(m)
 
 end
-    
-
+"""
 for tour in sous_tour
         complément = setdiff(1:n, tour)
         @constraint(m, sum(x[i,j] for i in tour, j in complément) >= sum(d[i] for i in tour) / C)
     end
+"""
 
-function main()
+list_name = ["n_5-euclidean_false","n_5-euclidean_true",
+                "n_6-euclidean_false","n_6-euclidean_true",
+                "n_7-euclidean_false","n_7-euclidean_true",
+                "n_8-euclidean_false","n_8-euclidean_true",
+                "n_9-euclidean_false","n_9-euclidean_true",
+                "n_10-euclidean_false","n_10-euclidean_true",
+                "n_11-euclidean_false","n_11-euclidean_true",
+                "n_12-euclidean_false","n_12-euclidean_true",
+                "n_13-euclidean_false","n_13-euclidean_true",
+                "n_14-euclidean_false","n_14-euclidean_true",
+                "n_15-euclidean_false","n_15-euclidean_true",
+                "n_16-euclidean_false","n_16-euclidean_true",]
+liste_reduite = ["n_5-euclidean_true"]
+
+function main(list_name)
     results = []
     global_start = time()
-    for file_name in ["n_6-euclidean_false"]
+    for file_name in list_name
         if occursin("euclidean", file_name)
             start = time()           
             println("Fichier : ", file_name)
--- statique.jl
+++ statique.jl
@@ -8,8 +8,9 @@ function statique(file_name::String)
     include(file_acces)
 
     # Créer le modèle
-    #m = Model(CPLEX.Optimizer)
-    m = Model(GLPK.Optimizer)
+    m = Model(CPLEX.Optimizer)
+    set_optimizer_attribute(m, "CPX_PARAM_TILIM", 180)
+    #m = Model(GLPK.Optimizer)
 
     @variable(m, x[1:n, 1:n], Bin);
     @variable(m, 0 <= u[1:n] );
@@ -36,7 +37,7 @@ function statique(file_name::String)
     if feasibleSolutionFound
         # Récupération des valeurs d’une variable
         #println("Valeur de l’objectif : ", JuMP.objective_value(m))
-        return JuMP.objective_value(m)
+        return JuMP.objective_value(m), MOI.get(m, MOI.RelativeGap())
     else
         return -1
 end
@@ -49,11 +50,67 @@ function main()
         if occursin("euclidean", file_name)
             start = time()           
             println("Fichier : ", file_name)
-            obj = statique(file_name)
+            obj , gap= statique(file_name)
             exec_time = time() - start
             global_exec_time = time() - global_start
-            push!(results, (file_name, obj, exec_time, global_exec_time))
+            push!(results, (file_name, obj,gap, exec_time, global_exec_time))
         end
     end
     println(results)
 end
+
+
+
+list_name = ["n_5-euclidean_false","n_5-euclidean_true",
+                "n_6-euclidean_false","n_6-euclidean_true",
+                "n_7-euclidean_false","n_7-euclidean_true",
+                "n_8-euclidean_false","n_8-euclidean_true",
+                "n_9-euclidean_false","n_9-euclidean_true",
+                "n_10-euclidean_false","n_10-euclidean_true",
+                "n_11-euclidean_false","n_11-euclidean_true",
+                "n_12-euclidean_false","n_12-euclidean_true",
+                "n_13-euclidean_false","n_13-euclidean_true",
+                "n_14-euclidean_false","n_14-euclidean_true",
+                "n_15-euclidean_false","n_15-euclidean_true",
+                "n_16-euclidean_false","n_16-euclidean_true",
+                "n_17-euclidean_false","n_17-euclidean_true",
+                "n_18-euclidean_false","n_18-euclidean_true",
+                "n_19-euclidean_false","n_19-euclidean_true",
+                "n_20-euclidean_false","n_20-euclidean_true",
+                "n_25-euclidean_false","n_25-euclidean_true",
+                "n_30-euclidean_false","n_30-euclidean_true",
+                "n_35-euclidean_false","n_35-euclidean_true",
+                "n_40-euclidean_false","n_40-euclidean_true",
+                "n_45-euclidean_false","n_45-euclidean_true",
+                "n_50-euclidean_false","n_50-euclidean_true",
+                "n_55-euclidean_false","n_55-euclidean_true",
+                "n_60-euclidean_false","n_60-euclidean_true",
+                "n_65-euclidean_false","n_65-euclidean_true",
+                "n_70-euclidean_false","n_70-euclidean_true",
+                "n_75-euclidean_false","n_75-euclidean_true",
+                "n_80-euclidean_false","n_80-euclidean_true",
+                "n_85-euclidean_false","n_85-euclidean_true",
+                "n_90-euclidean_false","n_90-euclidean_true",
+                "n_95-euclidean_false","n_95-euclidean_true",
+                "n_100-euclidean_false","n_100-euclidean_true",
+                ]
+liste_reduite = ["n_5-euclidean_true"]
+function main(liste)
+    results = []
+    global_start = time()
+    for file_name in liste
+        if occursin("euclidean", file_name)
+            start = time()           
+            println("Fichier : ", file_name)
+            gap = statique(file_name)
+            exec_time = time() - start
+            global_exec_time = time() - global_start
+            push!(results, (file_name, gap, exec_time, global_exec_time))
+        end
+    end
+    fout = open("output_statique.txt", "w")
+    # Ecrire "test" dans ce fichier
+    println(fout, results)
+    close(fout)
+end
+  
